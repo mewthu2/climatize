f5e833db8aeb3e423d36f2c2882cb164ac07234f
danielkbr <danielkbr89@gmail.com>
2024-05-06 00:36:23 -0300

    fix: test force schema

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
         Schema::disableForeignKeyConstraints();
-        URL::forceScheme('http');
+        URL::forceScheme('https');
     }
 }
