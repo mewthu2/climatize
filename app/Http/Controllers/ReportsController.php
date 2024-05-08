8b9d6f7269c815773bb39e767678d2e24fd0e8ba
danielkbr <danielkbr89@gmail.com>
2024-05-08 16:47:12 -0300

    fix: class names

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Log6temperatura;
-use App\Models\Log12temperatura;
-use App\Models\Log24temperatura;
+use App\Models\Log6Temperatura;
+use App\Models\Log12Temperatura;
+use App\Models\Log24Temperatura;
 use App\Models\Freezer;
 use App\Models\Usuario;
 
@@ -20,17 +20,17 @@ class ReportsController extends Controller
         if (request()->ajax() && $intervalo && $etiqueta_ident_freezer) {
             switch ($intervalo) {
                 case 6:
-                    $logs = Log6temperatura::where('etiqueta_ident', $etiqueta_ident_freezer)
+                    $logs = Log6Temperatura::where('etiqueta_ident', $etiqueta_ident_freezer)
                                             ->where('cad_cliente_id', $cad_usuario->cad_cliente_id)
                                             ->get();
                     break;
                 case 12:
-                    $logs = Log12temperatura::where('etiqueta_ident', $etiqueta_ident_freezer)
+                    $logs = Log12Temperatura::where('etiqueta_ident', $etiqueta_ident_freezer)
                                             ->where('cad_cliente_id', $cad_usuario->cad_cliente_id)
                                             ->get();
                     break;
                 case 24:
-                    $logs = Log24temperatura::where('etiqueta_ident', $etiqueta_ident_freezer)
+                    $logs = Log24Temperatura::where('etiqueta_ident', $etiqueta_ident_freezer)
                                             ->where('cad_cliente_id', $cad_usuario->cad_cliente_id)
                                             ->get();
                     break;
