d69950a0a1a9019b4d1b8444393a813c9f84f4a9
danielkbr <danielkbr89@gmail.com>
2024-05-10 04:28:10 -0300

    fix: layout adjust

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,8 +1,8 @@
-<footer class="text-center text-white" style="background: #36384f;">
+<footer class="text-center text-white text-sm" style="background: #36384f;">
     <div class="bg-black/5 p-6 text-center">
         <a href="{{ route('dashboard') }}">
             <div class="text-center flex justify-center">
-                <span>© 2023 Copyright:</span>  <x-logo-climatize :width="30" :height="30"></x-logo-climatize> 4 Climatize
+                <span>© 2023 Copyright:</span>  <x-logo-climatize :width="20" :height="20"></x-logo-climatize> 4 Climatize
             </div>
         </a>
     </div>
